62839d8a98ece613f788241410b97148ff97227f
Chris Baker <cbaker@thoughtworks.com>
2016-10-05 15:21:01 +0200

    changing client behaviour

--- libre_client/main.go
+++ libre_client/main.go
@@ -23,16 +23,16 @@ func main() {
 
 	c := pb.NewLibreClient(conn)
 
-	saveReply, err := c.SaveBook(context.Background(), &pb.SaveBookRequest{Book: &pb.Book{Title: "Some Funky Book"}})
+	c.SaveBook(context.Background(), &pb.SaveBookRequest{Book: &pb.Book{Title: "Some Funky Book"}})
 
 	fetchReply, err := c.FetchBooks(context.Background(), &pb.Empty{})
 
 	fmt.Println(len(fetchReply.Books))
 
-	c.DeleteBook(context.Background(), &pb.DeleteBookRequest{Book: saveReply.Book})
+	// c.DeleteBook(context.Background(), &pb.DeleteBookRequest{Book: saveReply.Book})
 
-	fetchReply, err = c.FetchBooks(context.Background(), &pb.Empty{})
-	fmt.Println(len(fetchReply.Books))
+	//fetchReply, err = c.FetchBooks(context.Background(), &pb.Empty{})
+	//fmt.Println(len(fetchReply.Books))
 
 	// sr, err := c.Search(context.Background(), &pb.SearchRequest{Keyword: "978-0393338102"})
 
